dca40557f4683e623248ac0de83e5233cba607fb
Ravindra Singh <ravindrarajpoot9628172@gmail.com>
2021-03-05 19:01:41 +0530

    assignment 3

--- block-BNaabx/project/server.js
+++ block-BNaabx/project/server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var time = new Date();
 var fs = require('fs');
-console.log(fs);
-
 var sec = time.getSeconds();
 var min = time.getMinutes();
 var hour = time.getHours();
@@ -11,6 +9,7 @@ app.use((req, res, next) => {
   console.log(req.method, req.url, `${hour}:${min}:${sec}`);
   next();
 });
+
 app.use((req, res, next) => {
   req.body = {};
   var data = '';
@@ -26,19 +25,26 @@ app.use((req, res, next) => {
     next();
   });
 });
+
 app.use((req, res, next) => {
-  var extension = req.url.split('.').pop();
-  console.log(extension);
-  console.log('public' + req.url);
-  if (['png', 'jpg', 'jpeg', 'svg'].includes(extension)) {
-    res.setHeader('Content-Type', 'images/' + extension);
-    fs.createReadStream(__dirname + '/public' + req.url).pipe(res);
-  } else if (req.url === '/style.css') {
-    res.setHeader('Content-Type', 'text/css');
-    fs.readFileSync(__dirname + '/stylesheet/' + req.url, 'utf8');
-  }
-  next();
+  res.sendFile(__dirname + '/public' + req.url, (err) => {
+    if (err) return next();
+  });
 });
+
+// app.use((req, res, next) => {
+//   if (req.url.split('.').pop() === 'css') {
+//     console.log(req.url);
+//     res.setHeader('Content-Type', 'text/css');
+//     fs.createReadStream(__dirname + '/public' + req.url).pipe(res);
+//   } else if (['png', 'jpg', 'svg'].includes(req.url.split('.').pop())) {
+//     console.log(req.url);
+//     res.setHeader('Content-Type', 'images/', req.url.split('.').pop());
+//     fs.createReadStream(__dirname + '/public' + req.url).pipe(res);
+//   }
+//   next();
+// });
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
