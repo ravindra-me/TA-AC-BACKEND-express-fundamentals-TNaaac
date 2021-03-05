f9afbf721cce684c391ae261ef1371e400723ef9
Ravindra Singh <ravindrarajpoot9628172@gmail.com>
2021-03-05 09:45:13 +0530

    assignment 1

---
+++ block-BNaabv/project/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+
+app.use(logger('tiny'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/admin', (req, res, next) => {
+  next('uncaught error');
+});
+
+app.get('/', (req, res) => {
+  res.send(`<h2> Welcome to express`);
+});
+
+app.get('/about', (req, res) => {
+  res.send('My name is qwerty');
+});
+
+app.post('/form', (req, res) => {
+  res.json(req.body);
+});
+app.post('/json', (req, res) => {
+  res.send(req.body);
+});
+
+app.get('/users/:username', (req, res) => {
+  let username = req.params.username;
+  res.send(username);
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Page is not find');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
+
+app.listen(3000, () => {
+  console.log('listener is listening on port 3000');
+});
